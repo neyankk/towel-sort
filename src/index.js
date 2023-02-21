aa1c4d5a7c1d5534de23d8c3106438214a181348
Miroslav <miroslav.shevyrev@bk.ru>
2023-02-21 03:09:26 +0300

    feat:decided

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+    var n = matrix.length, i = Math.floor(n/2);
+    while (i > 0)
+     { for (var j = 0; j < n; j++)
+        { var k = j, t = matrix[j];
+          while (k >= i && matrix[k-i] > t)
+           { matrix[k] = matrix[k-i]; k -= i; }
+          matrix[k] = t;
+        }
+      i = (i==2) ? 1 : Math.floor(i*5/11);
+     }
+  return matrix;
 }
